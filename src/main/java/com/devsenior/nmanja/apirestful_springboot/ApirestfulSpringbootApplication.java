12583d546b6f0dfcf2108d2c40a8d750232d24d7
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-06-10 21:28:42 -0500

    initial commit

---
+++ src/main/java/com/devsenior/nmanja/apirestful_springboot/ApirestfulSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.apirestful_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/nmanja/apirestful_springboot/ApirestfulSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.apirestful_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
